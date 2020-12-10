019b1a65f1330571ee0dcc351b1852f326c8f711
LAPTOP-5PT00PJJ\jason <596908546@qq.com>
2020-12-10 11:45:31 +0800

    no message

--- src/api/module/user.js
+++ src/api/module/user.js
@@ -2,8 +2,7 @@ import request from '../request'
 
 export const login = function () {
   /**
-   *  这个是百度的免费接口，仅供测试
-   *  正式使用时请自行换成业务接口
+   *  模拟...
    **/
-  return request.get('https://www.tianqiapi.com/api?version=v9&appid=23035354&appsecret=8YvlPNrz')
+  return Promise.resolve({ token: 'template-vue' })
 }
